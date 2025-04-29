3abcb7e292694904e7837ff3f209ae1f41dec1de
Manu <manuela.vitorino@academico.ifpb.edu.br>
2025-04-29 10:19:50 -0300

    importar o usuario

--- src/app/usuario/page.js
+++ src/app/usuario/page.js
@@ -1,6 +1,18 @@
-export default () => (
-<>
-    <h1>presidente da Republica</h1>
-    <p>{3+10}</p>
-    </>
-)
+import db from "@/lib/db"
+export default async () => {
+    const usuarios = await db.query("select * from usuario")
+ return (<>
+    <h1>Lista de usuario</h1>
+    <div>
+      {
+         usuarios.rows.map( 
+            a => (
+               <div>
+                  {a.nome} faz parte do projeto {a.cargo}
+               </div>
+            ) 
+         )
+      }
+   </div>
+ </>);
+}
